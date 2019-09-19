8716c3fb1f97cb49e7af827432f10f0714de9691
chris <chris@LAPTOP-T9OFFMSV>
2019-09-19 17:46:01 -0400

    <added changes>

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Christopher Boehm
+//Methods
+//9/19/2019
+
+using System;
 
 namespace Methods
 {
     class Program
     {
         static void Main(string[] args)
-        {
-        }
-    }
+            {
+            
+            //calls the function get_Name
+            get_Name();
+
+            //pauses before the program ends 
+            Console.ReadKey(true);
+            }
+       
+        //creates method
+        private static void get_Name()
+            {
+            // asks user to input name
+            Console.WriteLine("Please enter your name:  ");
+
+            string input = Console.ReadLine();
+
+            //writes to screen the hello plus the users name
+            Console.WriteLine("Hello " + input + "!");
+            }    
+   }
+    
 }
+
